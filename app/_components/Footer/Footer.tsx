9a1a0f3a15da34e646d439ee6e75534a75d3d7da
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-10-25 23:28:45 +0900

    feat: 공백 설정

--- app/_components/Footer/Footer.tsx
+++ app/_components/Footer/Footer.tsx
@@ -1,46 +1,46 @@
 import FooterItem from './FooterItem';
 
 const Footer = () => {
-	return (
-		<div
-			className="fixed bottom-0 mt-2 box-border flex h-[69px] min-w-full justify-between rounded-t-2xl bg-[#F9F9F9] p-2 px-[30px] "
-			style={{ boxShadow: '0px -3px 5px -2px lightgray' }}
-		>
-			<FooterItem
-				iconPath={
-					<path
-						strokeLinecap="round"
-						strokeLinejoin="round"
-						d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z"
-					/>
-				}
-				labelName={'주문내역'}
-				to={'/order'}
-			/>
-			<FooterItem
-				iconPath={
-					<path
-						strokeLinecap="round"
-						strokeLinejoin="round"
-						d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-					/>
-				}
-				labelName={'홈'}
-				to={'/home'}
-			/>
-			<FooterItem
-				iconPath={
-					<path
-						strokeLinecap="round"
-						strokeLinejoin="round"
-						d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-					/>
-				}
-				labelName={'마이페이지'}
-				to={'/mypage'}
-			/>
-		</div>
-	);
+  return (
+    <div
+      className="fixed bottom-0 mt-2 box-border flex h-[69px] min-w-full justify-between rounded-t-2xl bg-[#F9F9F9] p-2 px-[30px] "
+      style={{ boxShadow: '0px -3px 5px -2px lightgray' }}
+    >
+      <FooterItem
+        iconPath={
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z"
+          />
+        }
+        labelName={'주문내역'}
+        to={'/order'}
+      />
+      <FooterItem
+        iconPath={
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+          />
+        }
+        labelName={'홈'}
+        to={'/home'}
+      />
+      <FooterItem
+        iconPath={
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+          />
+        }
+        labelName={'마이페이지'}
+        to={'/mypage'}
+      />
+    </div>
+  );
 };
 
 export default Footer;
