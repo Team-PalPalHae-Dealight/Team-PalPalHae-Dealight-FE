8a4c4b1b304a2393c1b72fb74e3b29dde19f8a33
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-10-25 23:31:12 +0900

    feat: 들어쓰기 확인 커밋

--- app/_components/Footer/FooterItem.tsx
+++ app/_components/Footer/FooterItem.tsx
@@ -3,34 +3,34 @@ import { useRouter } from 'next/navigation';
 import { ReactElement } from 'react';
 
 type FooterItemPropType = {
-	iconPath: ReactElement;
-	labelName: string;
-	to: string;
+  iconPath: ReactElement;
+  labelName: string;
+  to: string;
 };
 
 const FooterItem = ({ iconPath, labelName, to }: FooterItemPropType) => {
-	const router = useRouter();
+  const router = useRouter();
 
-	return (
-		<div
-			className="group flex cursor-pointer flex-col items-center justify-center"
-			onClick={() => router.push(to)}
-		>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				strokeWidth="1.5"
-				stroke="#7D7D7D"
-				className="h-6 w-full group-hover:stroke-[#FFE429]"
-			>
-				{iconPath}
-			</svg>
-			<label className="cursor-pointer group-hover:text-[#FFE429]">
-				{labelName}
-			</label>
-		</div>
-	);
+  return (
+    <div
+      className="group flex cursor-pointer flex-col items-center justify-center"
+      onClick={() => router.push(to)}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="#7D7D7D"
+        className="h-6 w-full group-hover:stroke-[#FFE429]"
+      >
+        {iconPath}
+      </svg>
+      <label className="cursor-pointer group-hover:text-[#FFE429]">
+        {labelName}
+      </label>
+    </div>
+  );
 };
 
 export default FooterItem;
