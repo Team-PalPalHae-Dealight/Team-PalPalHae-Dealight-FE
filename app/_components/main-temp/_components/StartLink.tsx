c8d8319ed546ca1551fb314c69e0b1ecc3be0924
jgjgill <dbdltm22@naver.com>
2023-10-26 15:09:26 +0900

    style: shadow 추가

--- app/_components/main-temp/_components/StartLink.tsx
+++ app/_components/main-temp/_components/StartLink.tsx
@@ -8,37 +8,37 @@ import Link from 'next/link';
  */
 
 const StartLink = () => {
-	return (
-		<div className="flex flex-col gap-14 p-12">
-			<Link
-				className="flex items-center justify-center rounded-2xl border-2 border-white bg-white p-8 text-2xl transition hover:border-yellow-300"
-				href="/"
-			>
-				<Image
-					src="/shop-main.png"
-					width={58}
-					height={57}
-					priority
-					alt="shop main"
-				/>
-				<b className="ml-5 font-semibold">사장 &nbsp;</b> 으로 시작
-			</Link>
+  return (
+    <div className="flex flex-col gap-14 p-12">
+      <Link
+        className="flex items-center justify-center rounded-2xl border-2 border-white bg-white p-8 text-2xl shadow transition hover:border-yellow-300"
+        href="/"
+      >
+        <Image
+          src="/shop-main.png"
+          width={58}
+          height={57}
+          priority
+          alt="shop main"
+        />
+        <b className="ml-5 font-semibold">사장 &nbsp;</b> 으로 시작
+      </Link>
 
-			<Link
-				className="flex items-center justify-center rounded-2xl border-2 border-white bg-white p-8 text-2xl transition hover:border-yellow-300"
-				href="/"
-			>
-				<Image
-					src="/user-main.png"
-					width={58}
-					height={57}
-					priority
-					alt="shop main"
-				/>
-				<b className="ml-5 font-semibold">고객 &nbsp;</b> 으로 시작
-			</Link>
-		</div>
-	);
+      <Link
+        className="flex items-center justify-center rounded-2xl border-2 border-white bg-white p-8 text-2xl shadow transition hover:border-yellow-300"
+        href="/"
+      >
+        <Image
+          src="/user-main.png"
+          width={58}
+          height={57}
+          priority
+          alt="shop main"
+        />
+        <b className="ml-5 font-semibold">고객 &nbsp;</b> 으로 시작
+      </Link>
+    </div>
+  );
 };
 
 export default StartLink;
