0013e7f543fca7584b9819365c69cb8103306004
jgjgill <dbdltm22@naver.com>
2023-10-26 21:32:26 +0900

    fix: staticDirs 경로 설정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,23 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
-	stories: ['../app/**/*.mdx', '../app/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-	addons: [
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'@storybook/addon-onboarding',
-		'@storybook/addon-interactions',
-	],
-	framework: {
-		name: '@storybook/nextjs',
-		options: {},
-	},
-	docs: {
-		autodocs: 'tag',
-	},
-	staticDirs: ['../public'],
+  stories: ['../app/**/*.mdx', '../app/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  staticDirs:
+    process.env.NODE_ENV === 'production'
+      ? [{ from: '../public', to: '/public' }]
+      : ['../public'],
 };
 export default config;
