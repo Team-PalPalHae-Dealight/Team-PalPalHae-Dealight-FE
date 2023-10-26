8f1cb497b07e95543c4aebf2324ed7fcab3fdc2e
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-10-27 02:18:04 +0900

    fix: storybook 배포 test

--- app/_components/primaryButton/PrimaryButton.stories.tsx
+++ app/_components/primaryButton/PrimaryButton.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import PrimaryButton from './PrimaryButton';
+import PrimaryButton from './primaryButton';
 
 const meta: Meta<typeof PrimaryButton> = {
   title: 'Components/PrimaryButton',
