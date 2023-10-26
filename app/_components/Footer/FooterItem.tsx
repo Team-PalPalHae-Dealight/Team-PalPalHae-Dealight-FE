453f1a7f17325dd39820d72811a20858495bdc1e
JongHyun <108605838+jonghyunlee95@users.noreply.github.com>
2023-10-26 14:29:32 +0900

    footer 컴포넌트 작업 (#22)
    * feat: Footer컴포넌트 구현
    
    * style: 기본 css 삭제
    
    * style: 기본 색상 변경
    
    * style: prettier 설정 변경
    
    * feat: 프리티어 설정 변경
    
    * feat: 공백 설정
    
    * feat: 공백 확인을 위한 커밋
    
    * feat: 들어쓰기 확인 커밋
    
    * feat: 타입 변경
    
    * feat: storybook 구현
    
    * fix: 스토리북 에러 작업
    nextjs app 경로 관련 설정 추가
    
    ---------
    
    Co-authored-by: jgjgill <dbdltm22@naver.com>

---
+++ app/_components/Footer/FooterItem.tsx
@@ -0,0 +1,36 @@
+'use client';
+import { useRouter } from 'next/navigation';
+import { ReactNode } from 'react';
+
+type FooterItemPropType = {
+  iconPath: ReactNode;
+  labelName: string;
+  to: string;
+};
+
+const FooterItem = ({ iconPath, labelName, to }: FooterItemPropType) => {
+  const router = useRouter();
+
+  return (
+    <div
+      className="group flex cursor-pointer flex-col items-center justify-center"
+      onClick={() => router.push(to)}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="#7D7D7D"
+        className="h-6 w-full group-hover:stroke-[#FFE429]"
+      >
+        {iconPath}
+      </svg>
+      <label className="cursor-pointer group-hover:text-[#FFE429]">
+        {labelName}
+      </label>
+    </div>
+  );
+};
+
+export default FooterItem;
