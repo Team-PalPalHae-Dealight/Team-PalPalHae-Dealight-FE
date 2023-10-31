ab4df4bb6037e3bb503470dda817292caaf9f05e
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-11-01 03:52:23 +0900

    refactor: Footer컴포넌트 height수정

--- app/_components/Footer/Footer.tsx
+++ app/_components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import FooterItem from './FooterItem';
 const Footer = () => {
   return (
     <div
-      className="h-17 sticky bottom-0 mt-2 box-border flex w-full justify-between rounded-t-2xl bg-light-gray px-8 py-2"
+      className="sticky bottom-0 mt-2 box-border flex h-16 w-full justify-between rounded-t-2xl bg-light-gray px-8 py-2"
       style={{ boxShadow: '0px -3px 5px -2px rgb(0, 0, 0, 0.1)' }}
     >
       <FooterItem icon={'Order'} labelName={'주문내역'} to={'/'} />
