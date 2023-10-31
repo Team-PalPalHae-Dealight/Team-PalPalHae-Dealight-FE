8e61a703061f480f50c01f65adc5d739acdff036
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-11-01 03:54:16 +0900

    refactor: PrimaryButton버튼 스토리북 수정

--- app/_components/PrimaryButton/PrimaryButton.stories.tsx
+++ app/_components/PrimaryButton/PrimaryButton.stories.tsx
@@ -9,7 +9,7 @@ const meta: Meta<typeof PrimaryButton> = {
 export default meta;
 type Story = StoryObj<typeof PrimaryButton>;
 
-export const Default: Story = {
+export const Primary: Story = {
   render: () => (
     <div className="fixed left-5 top-5 w-96">
       <PrimaryButton onClick={() => 'clicked'}>버튼</PrimaryButton>
