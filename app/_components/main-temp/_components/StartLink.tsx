ad2dbe346e5dfc05b1831eb907f00828dc3bbe82
jgjgill <dbdltm22@naver.com>
2023-11-01 15:18:43 +0900

    시작 링크 관련 작업 잘못된 라우트명 수정 msw 관련 설정 변경

--- app/_components/main-temp/_components/StartLink.tsx
+++ app/_components/main-temp/_components/StartLink.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import ShopMain from '../../../_assets/images/shop-main.png';
 import UserMain from '../../../_assets/images/user-main.png';
+import pageRoute from '@/app/_constants/route';
 
 /**
  * @todo 라우팅 관련 네이밍 정해지면 href 변경
@@ -11,21 +12,37 @@ import UserMain from '../../../_assets/images/user-main.png';
 
 const StartLink = () => {
   return (
-    <div className="flex flex-col gap-14 p-12">
+    <div className="mt-2.5 flex w-full gap-2">
       <Link
-        className="flex items-center justify-center rounded-2xl border-2 border-white bg-white p-8 text-2xl shadow transition hover:border-yellow-300"
-        href="/"
+        className="relative w-full rounded-lg bg-white px-1.5 py-3.5 shadow"
+        href={pageRoute.store.login()}
       >
-        <Image src={ShopMain} width={58} height={57} priority alt="shop main" />
-        <b className="ml-5 font-semibold">사장 &nbsp;</b> 으로 시작
+        <b className="text-xl font-semibold">사장</b>
+        <p className="mt-2.5 text-xs">
+          사장님으로 <br /> 활동하기
+        </p>
+        <Image
+          src={ShopMain}
+          priority
+          alt="shop main"
+          className="absolute bottom-0 right-0"
+        />
       </Link>
 
       <Link
-        className="flex items-center justify-center rounded-2xl border-2 border-white bg-white p-8 text-2xl shadow transition hover:border-yellow-300"
-        href="/"
+        className="relative w-full rounded-lg bg-white px-1.5 py-3.5 shadow"
+        href={pageRoute.customer.login()}
       >
-        <Image src={UserMain} width={58} height={57} priority alt="user main" />
-        <b className="ml-5 font-semibold">고객 &nbsp;</b> 으로 시작
+        <b className="text-xl font-semibold">고객</b>
+        <p className="mt-2.5 text-xs">
+          고객님으로 <br /> 활동하기
+        </p>
+        <Image
+          src={UserMain}
+          priority
+          alt="user main"
+          className="absolute bottom-0 right-0"
+        />
       </Link>
     </div>
   );
