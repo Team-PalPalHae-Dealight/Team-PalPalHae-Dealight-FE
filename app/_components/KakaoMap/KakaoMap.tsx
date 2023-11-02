a847594c93fb2d1c624b8b2f66268872d0032022
jgjgill <dbdltm22@naver.com>
2023-11-02 17:15:02 +0900

    feat: 마커 추가, 삭제 관련 기능 구현

--- app/_components/KakaoMap/KakaoMap.tsx
+++ app/_components/KakaoMap/KakaoMap.tsx
@@ -1,31 +1,28 @@
 'use client';
 
-import { useEffect } from 'react';
+import useGetMarker from '@/app/_hooks/kakao-map/useGetMakers';
+import useGetMap from '@/app/_hooks/kakao-map/useGetMap';
 
 type KakaoMapPropsType = {
   width?: string;
   height?: string;
   lat?: number;
   lng?: number;
+  positions?: { lat: number; lng: number; title: string }[];
 };
 
 const KakaoMap = ({
   width = '100%',
   height = '140px',
-  lat = 37.566826,
-  lng = 126.9786567,
+  lat = 33.450701,
+  lng = 126.570667,
+  positions = [
+    // { lat: 33.450705, lng: 126.56956, title: '카카오' },
+    { lat: 33.450936, lng: 126.569477, title: '생태연못' },
+  ],
 }: KakaoMapPropsType) => {
-  useEffect(() => {
-    window.kakao.maps.load(() => {
-      const container = document.getElementById('map');
-      const options = {
-        center: new window.kakao.maps.LatLng(lat, lng),
-        level: 3,
-      };
-
-      new window.kakao.maps.Map(container, options);
-    });
-  }, [lat, lng]);
+  const map = useGetMap({ lat, lng, positionCount: positions.length });
+  useGetMarker({ positions, map });
 
   return (
     <div className="w-full">
---
+++ app/_hooks/kakao-map/useGetMakers.ts
@@ -0,0 +1,35 @@
+import { useEffect } from 'react';
+
+const IMAGE_MARKER_URL =
+  'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png';
+
+type UseGetMarkerPropsType = {
+  positions: { lat: number; lng: number; title: string }[];
+  map: object;
+};
+
+const useGetMarker = ({ positions, map }: UseGetMarkerPropsType) => {
+  useEffect(() => {
+    if (map === undefined) return;
+
+    positions.forEach(({ lat, lng, title }) => {
+      const mapPosition = new window.kakao.maps.LatLng(lat, lng);
+
+      const imageSize = new window.kakao.maps.Size(24, 35);
+      const markerImage = new window.kakao.maps.MarkerImage(
+        IMAGE_MARKER_URL,
+        imageSize
+      );
+
+      const marker = new window.kakao.maps.Marker({
+        title,
+        position: mapPosition,
+        image: markerImage,
+      });
+      marker.setMap(map);
+      console.log(marker);
+    });
+  }, [map, positions]);
+};
+
+export default useGetMarker;
