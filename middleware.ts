4d577be09ddf476e8e59c0059d6054914aded9c2
euan-lee <maplestory1419@naver.com>
2023-11-02 13:44:50 +0900

    feat: middleware 리다이렉트 구현중

--- middleware.ts
+++ middleware.ts
@@ -1,32 +1,52 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-
 const middleware = async (request: NextRequest) => {
   try {
     const accessToken = 1;
     const refreshToken = 2;
-    if (
-      accessToken === 1 &&
-      refreshToken === 2 &&
-      request.url.includes('/login') === false
-    ) {
-      if (request.url.includes('/customer')) {
-        return NextResponse.redirect(
-          new URL('http://localhost:3000/customer/login')
-        );
-      }
-      if (request.url.includes('/store')) {
-        console.log('request.url', request.url);
+    switch (request.url.includes('/customer')) {
+      case request.url.includes('/cart'):
+        break;
+      case request.url.includes('/home'):
+        break;
+      case request.url.includes('/login'):
+        break;
+      case request.url.includes('/my-page'):
+        break;
+      case request.url.includes('/order-detail'):
+        break;
+      case request.url.includes('/order-list'):
+        break;
+      case request.url.includes('/order-product'):
+        break;
+      case request.url.includes('/order-result'):
+        break;
+      case request.url.includes('/review'):
+        break;
+      case request.url.includes('/review-write'):
+        break;
+      case request.url.includes('/search'):
+        break;
+      case request.url.includes('/sign-up'):
+        break;
+      case request.url.includes('/store-detail'):
+        break;
+    }
+    if (request.url.includes('/store')) {
+      if (refreshToken !== null && request.url.includes('/login'))
+        //로그인 하고 로그인 페이지 접근시
+        return NextResponse.redirect(new URL(request.url));
+      if (accessToken === null && refreshToken === null) {
         return NextResponse.redirect(
           new URL('http://localhost:3000/store/login')
         );
+      } else if (accessToken === null && refreshToken !== null) {
+        //refresh token 전송
+        //받은 토큰 로컬 스토리지에 저장
+        //저장한 토큰 헤더에 담아서 전송
+      } else if (accessToken !== null && refreshToken !== null) {
+        //헤더에 토큰 담아서 전송
       }
-    }
-    if (accessToken !== 1 && refreshToken === 2) {
-      //토큰 저장
-      //서버에 보내기
-      //받은 refresh token 저장하기
-      //저장한 토큰 다시 헤더에 담기
     }
   } catch (error) {
     console.error(error);
