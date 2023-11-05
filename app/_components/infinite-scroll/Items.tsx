b4c018ec5c3fd1efbe4c6397fb67d33dce901f1c
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-11-06 00:20:42 +0900

    refactor: url이 변경되고 뒤로가기로 돌아왔을때 이전 Y좌표로 이동하는 기능 추가

--- app/_components/infinite-scroll/Items.tsx
+++ app/_components/infinite-scroll/Items.tsx
@@ -9,12 +9,15 @@ export type ItemTypes = {
 
 const Items = ({ items }: ItemTypes) => {
   // 리스트 카드
+  const onClickCardLink = () => {
+    sessionStorage.setItem('scrollY', JSON.stringify(window.scrollY));
+  };
   return (
     <>
       {items ? (
         items.map(item => {
           return (
-            <Link key={item.id} href={'/asd'}>
+            <Link key={item.id} href={'/asd'} onClick={onClickCardLink}>
               <div className="m-2 h-14 border-2 border-black">
                 <div>{item.id}</div>
               </div>
