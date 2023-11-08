caa11ccf08980cb91234556569cbbbdf46d3e184
euan-lee <maplestory1419@naver.com>
2023-11-08 18:17:28 +0900

    feat: axios contextapi설정

---
+++ app/_providers/auth.tsx
@@ -0,0 +1,27 @@
+import {
+  ReactComponentElement,
+  ReactNode,
+  createContext,
+  useContext,
+  useState,
+} from 'react';
+import React from 'react';
+
+const LogContext = createContext({ loggedin: false, loginToggle: () => {} });
+
+export const LogContextProvider = ({ children }: { children: ReactNode }) => {
+  const [loggedin, setLogin] = useState(false);
+  const loginToggle = () => {
+    setLogin(prev => !prev);
+  };
+
+  return (
+    <LogContext.Provider value={{ loggedin, loginToggle }}>
+      {children}
+    </LogContext.Provider>
+  );
+};
+
+export const useLogin = () => {
+  return useContext(LogContext);
+};
---
+++ app/_services/auth/auth.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { getStorage } from '@/app/_utils/localstorage';
+
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: '/',
+  },
+});
+
+axiosInstance.interceptors.request.use(config => {
+  const token = getStorage('accessToken');
+  if (token !== null) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
