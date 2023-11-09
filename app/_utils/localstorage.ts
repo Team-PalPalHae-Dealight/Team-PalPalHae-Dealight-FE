e1c5ac081d1e3e2d75b145817fc333f7f0992611
Lim_JiSeon <dpf226@naver.com>
2023-11-09 22:10:31 +0900

    refactor: 불필요한 코드 삭제

--- app/_utils/localstorage.ts
+++ app/_utils/localstorage.ts
@@ -19,28 +19,3 @@ const LocalStorage = {
 };
 
 export default LocalStorage;
-
-// class LocalStorage {
-//   constructor() {}
-//   //eslint-disable-next-line
-//   static setItem(key: string, value: any) {
-//     if (typeof window !== 'undefined') {
-//       localStorage.setItem(key, JSON.stringify(value));
-//     }
-//   }
-
-//   static getItem(key: string) {
-//     if (typeof window !== 'undefined') {
-//       return localStorage.getItem(key) || '{}';
-//     }
-//     return null;
-//   }
-
-//   static removeItem(key: string) {
-//     if (typeof window !== 'undefined') {
-//       localStorage.removeItem(key);
-//     }
-//   }
-// }
-
-// export default LocalStorage;
