07cf6dba8798aca56ddf7c008277458389fb3b9c
jgjgill <dbdltm22@naver.com>
2023-11-09 15:02:46 +0900

    fix: 지도 관련 에러 작업

--- app/_hooks/kakao-map/useGetMap.ts
+++ app/_hooks/kakao-map/useGetMap.ts
@@ -1,25 +1,33 @@
 import { useEffect, useState } from 'react';
 
-type UseGetMapPropsType = { lat: number; lng: number; positionCount: number };
+type UseGetMapPropsType = { lat: number; lng: number };
 
 const useGetMap = ({
   lat = 37.566826,
   lng = 126.9786567,
-  positionCount,
 }: UseGetMapPropsType) => {
   const [map, setMap] = useState<React.ReactNode>();
 
   useEffect(() => {
-    window.kakao.maps.load(() => {
-      const container = document.getElementById('map');
-      const options = {
-        center: new window.kakao.maps.LatLng(lat, lng),
-        level: 3,
-      };
+    const kakaoMapScript = document.createElement('script');
+    kakaoMapScript.async = false;
+    kakaoMapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&autoload=false&libraries=services`;
+    document.head.appendChild(kakaoMapScript);
 
-      setMap(new window.kakao.maps.Map(container, options));
-    });
-  }, [lat, lng, positionCount]);
+    const onLoadKakaoAPI = () => {
+      window.kakao.maps.load(() => {
+        const container = document.getElementById('map');
+        const options = {
+          center: new window.kakao.maps.LatLng(lat, lng),
+          level: 3,
+        };
+
+        setMap(new window.kakao.maps.Map(container, options));
+      });
+    };
+
+    kakaoMapScript.addEventListener('load', onLoadKakaoAPI);
+  }, [lat, lng]);
 
   return map;
 };
