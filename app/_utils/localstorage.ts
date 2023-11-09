2a346f03d3e94ea4e5b34ff47ef994977fecf0fc
Lim-JiSeon <83554018+Lim-JiSeon@users.noreply.github.com>
2023-11-10 05:18:13 +0900

    색상 코드 추가 및 jsdoc 추가 (#114)
    * feat: localstorage 네이밍 컨벤션 추가
    
    * feat: tailwind 색상 추가

--- app/_utils/localstorage.ts
+++ app/_utils/localstorage.ts
@@ -1,3 +1,9 @@
+/**
+ * @namespace 로컬스토리지 key name : dealight-변수명
+ * 변수명 컨벤션 : Camel case
+ * ex) dealight-storeId
+ */
+
 const LocalStorage = {
   //eslint-disable-next-line
   setItem: (key: string, value: any) => {
