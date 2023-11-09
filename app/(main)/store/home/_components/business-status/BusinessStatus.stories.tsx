6768bad48265e4dd02bbdf8713bdb591a6e8ee0a
Lim-JiSeon <83554018+Lim-JiSeon@users.noreply.github.com>
2023-11-09 21:48:01 +0900

    영업 상태 토글 컴포넌트 구현하기 (#104)
    * feat: 토글 컴포넌트 구현
    
    * feat: 영업 상태 변경 컴포넌트 구현
    
    * fix: storybook 배포 에러 해결
    
    * refactor: 깃허브 피드백 반영

---
+++ app/(main)/store/home/_components/business-status/BusinessStatus.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import BusinessStatus from './BusinessStatus';
+
+const meta: Meta<typeof BusinessStatus> = {
+  title: 'Components/BusinessStatus',
+  component: BusinessStatus,
+};
+
+export default meta;
+type Story = StoryObj<typeof BusinessStatus>;
+
+export const Primary: Story = {
+  render: () => <BusinessStatus />,
+};
