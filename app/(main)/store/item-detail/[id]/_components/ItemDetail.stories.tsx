786cfa295e80f2d37f0b4cfa2387f9862fab6441
jgjgill <dbdltm22@naver.com>
2023-11-13 19:59:14 +0900

    chore: 임시 컴포넌트 이름 변경

--- app/(main)/store/item-detail/[id]/_components/ItemDetail.stories.tsx
+++ app/(main)/store/item-detail/[id]/_components/ItemDetail.stories.tsx
@@ -1,19 +1,19 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import ItemDetail from './ItemDetail';
+import ItemDetailTemp from './ItemDetailTemp';
 
-const meta: Meta<typeof ItemDetail> = {
-  title: 'Components/ItemDetail',
-  component: ItemDetail,
+const meta: Meta<typeof ItemDetailTemp> = {
+  title: 'Components/ItemDetailTemp',
+  component: ItemDetailTemp,
   tags: ['autodocs'],
 };
 
 export default meta;
-type Story = StoryObj<typeof ItemDetail>;
+type Story = StoryObj<typeof ItemDetailTemp>;
 
 export const Primary: Story = {
   render: () => (
     <main className="flex flex-col items-center px-5 pt-2.5">
-      <ItemDetail />
+      <ItemDetailTemp />
     </main>
   ),
 };
