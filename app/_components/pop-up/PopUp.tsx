9694b61352957df18419182013c5573e16387b3f
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-11-14 04:20:54 +0900

    feat: subText font-weight수정

--- app/_components/pop-up/PopUp.tsx
+++ app/_components/pop-up/PopUp.tsx
@@ -18,8 +18,8 @@ const PopUp = ({
   return (
     <div className="fixed left-0 top-0 flex h-full w-full items-center justify-center bg-black bg-opacity-50">
       <div className="flex w-60 flex-col items-center rounded bg-white">
-        <div className="flex w-full flex-col items-center justify-center p-6 text-center text-sm font-semibold	">
-          <div>{mainText}</div>
+        <div className="flex w-full flex-col items-center justify-center p-6 text-center text-sm">
+          <div className="font-semibold">{mainText}</div>
           {subText && (
             <div className="mt-3 text-xs text-dark-gray">{subText}</div>
           )}
