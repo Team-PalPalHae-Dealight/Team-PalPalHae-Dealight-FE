dd8a1311f930c78dd6ff0c26cd8c9cfc7f45c8ce
jgjgill <dbdltm22@naver.com>
2023-11-13 16:40:34 +0900

    feat: 배포 환경 api 연결 테스트

--- app/_components/main-temp/ApiTest.tsx
+++ app/_components/main-temp/ApiTest.tsx
@@ -1,21 +1,25 @@
 'use client';
 
 async function postApi() {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/items?memberId=1`,
-    {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json;charset=UTF-8' },
-      body: JSON.stringify({
-        name: '떡볶이',
-        stock: 1,
-        discountPrice: 233,
-        originalPrice: 2,
-        description: null,
-        information: '안내',
-      }),
-    }
-  );
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/stores`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8',
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1MDAwMDAxIiwiaXNzIjoiREVBTElHSFQtQVBJLVNFUlZFUiIsImlhdCI6MTY5OTgzODEzOSwiZXhwIjoxNjk5OTI0NTM5LCJBdXRob3JpdGllcyI6IlJPTEVfTUVNQkVSIn0.wowAMfaK7bjn4XWhOmkBpgmlR-atvAHSx1klB6lNq8w',
+    },
+    body: JSON.stringify({
+      storeNumber: '888222111',
+      name: '맛짱조개',
+      telephone: '01066772291',
+      addressName: '서울시 강남구',
+      xCoordinate: 67.89,
+      yCoordinate: 293.2323,
+      openTime: '09:00:00',
+      closeTime: '23:00:00',
+      dayOff: ['월요일'],
+    }),
+  });
   const data = await response.json();
   if (!response.ok) {
     if (data.message) {
@@ -28,13 +32,14 @@ async function postApi() {
 }
 
 async function getApi() {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/stores/profiles/1/1`,
-    {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json;charset=UTF-8' },
-    }
-  );
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/items/1`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8',
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzMTQwMjU1OTk0IiwiaXNzIjoiREVBTElHSFQtQVBJLVNFUlZFUiIsImlhdCI6MTY5OTgzNjk2NSwiZXhwIjoxNjk5OTIzMzY1LCJBdXRob3JpdGllcyI6IlJPTEVfQURNSU4ifQ.GLyDsyX1x4mKMeGHOn8TSD4j3W_zQDTTxrJCLRSViNA',
+    },
+  });
   const data = await response.json();
 
   if (!response.ok) {
