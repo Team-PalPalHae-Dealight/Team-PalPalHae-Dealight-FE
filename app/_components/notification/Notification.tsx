631acc6d28bd972f6815905a665dee8c6ec7f7ed
jgjgill <79239852+jgjgill@users.noreply.github.com>
2023-11-14 14:30:53 +0900

    상품 관련 디자인 작업 (#132)
    * feat: 스토리북 관련 절대 경로 설정
    
    * chore: lint-staged 설정 변경
    
    * style: 상품 조회 페이지 디자인 작업
    
    * chore: 임시 컴포넌트 이름 변경
    
    * feat: 상품 수정 페이지 기본 디자인 구성
    
    * refactor: 어색한 경로 수정
    
    * feat: 상품 등록 페이지 기본 디자인 작업
    상품 수정 페이지에서의 디자인과 동일
    
    * refactor: Notification 컴포넌트 사용
    
    * chore: 폰트 스타일 수정

--- app/_components/notification/Notification.tsx
+++ app/_components/notification/Notification.tsx
@@ -5,12 +5,12 @@ type NotificationPropsType = {
 const Notification = ({ children }: NotificationPropsType) => {
   return (
     <div className="min-h-42.5 w-full rounded bg-white p-4">
-      <div className="pb-2.5 text-lg text-black">안내 사항</div>
-      <div className="pb-2 text-xs text-black">
+      <div className="pb-2.5 text-lg font-bold">안내 사항</div>
+      <div className="pb-2 text-xs">
         딜라잇(Dealight) 서비스를 이용해주셔서 감사합니다.
       </div>
-      <div className="pb-2 text-xs text-black">{children}</div>
-      <div className="text-xs text-black">
+      <div className="pb-2 text-xs">{children}</div>
+      <div className="text-xs">
         딜라잇(Dealight)은 상품 거래 예약을 도와주며, 그 외 과정에서 문제가
         발생할 경우 책임을 지지 않습니다.
       </div>
