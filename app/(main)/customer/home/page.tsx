eacbc54744ca33ad70c27af293ccccc2c05da588
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-11-16 03:20:47 +0900

    feat: 고객 측 홈 화면 오늘의 딜라잇 구현

--- app/(main)/customer/home/page.tsx
+++ app/(main)/customer/home/page.tsx
@@ -1,21 +1,31 @@
 'use client';
 
 import KakaoMap from '@/app/_components/KakaoMap/KakaoMap';
+import TodayDealight from '@/app/(main)/customer/home/_components/TodayDealight';
+import CustomerHeader from '@/app/_components/Header/CustomerHeader';
 
 export default function Page() {
   return (
-    <h1>
-      home
-      <div>
-        <KakaoMap
-          onClickCurrentPosition={() => console.log('현재 위치 클릭')}
-          onClickPosition={() => console.log('marker')}
-          positions={[
-            { lat: 33.450705, lng: 126.56956, title: '카카오' },
-            { lat: 33.450936, lng: 126.569477, title: '생태연못' },
-          ]}
-        />
+    <>
+      <CustomerHeader />
+      <div className="mx-5">
+        <div className="my-3 rounded">
+          <KakaoMap
+            onClickCurrentPosition={() => console.log('현재 위치 클릭')}
+            onClickPosition={() => console.log('marker')}
+            positions={[
+              { lat: 33.450705, lng: 126.56956, title: '카카오' },
+              { lat: 33.450936, lng: 126.569477, title: '생태연못' },
+            ]}
+          />
+        </div>
+        <div>
+          <TodayDealight
+            listName={'오늘의 딜라잇'}
+            emptyWord={'상품이 존재하지 않습니다.'}
+          />
+        </div>
       </div>
-    </h1>
+    </>
   );
 }
