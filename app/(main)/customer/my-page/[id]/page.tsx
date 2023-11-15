5d2fc576c508622bbabef144e918d4d5ceab5a41
Lim_JiSeon <dpf226@naver.com>
2023-11-16 00:59:25 +0900

    feat: 고객 마이페이지 구현

--- app/(main)/customer/my-page/[id]/page.tsx
+++ app/(main)/customer/my-page/[id]/page.tsx
@@ -1,4 +1,16 @@
+import CustomerHeader from '@/app/_components/Header/CustomerHeader';
+import MyPageContents from './_components/MyPageContents/MyPageContents';
+import Footer from '@/app/_components/Footer/Footer';
+
 export default function Page({ params }: { params: { slug: string } }) {
   console.log(params);
-  return <h1>My Page</h1>;
+  return (
+    <div className="flex flex-col items-center pt-2.5">
+      <CustomerHeader />
+      <div className="w-full px-5">
+        <MyPageContents />
+      </div>
+      <Footer />
+    </div>
+  );
 }
--- app/_constants/errorMessage.ts
+++ app/_constants/errorMessage.ts
@@ -4,4 +4,5 @@ export const ERROR_MESSAGE = {
   STORE_NUMBER: '숫자만 입력해주세요.',
   STORE_MIN: '최소 2글자 이상 입력해주세요.',
   STORE_ARRAY_MIN: '최소 1개 이상 선택해주세요.',
+  PHONE_NUMBER: '-을 제외한 숫자 11자리를 입력해주세요.',
 };
