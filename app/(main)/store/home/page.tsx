1eb221fd3b5ce3c276ad223f8a8d5c1cf03e6c46
JongHyun <108605838+jonghyunlee95@users.noreply.github.com>
2023-11-17 16:27:29 +0900

    업체 측 홈 페이지 상품 목록 컴포넌트 구현 (#149)
    * feat: 업체 측 홈 페이지 컴포넌트 구현
    
    * style: 마진 수정
    
    * refactor: orderList 수정
    
    * refactor: 최상위 layout width수정
    
    * refactor: 리뷰 반영

--- app/(main)/store/home/page.tsx
+++ app/(main)/store/home/page.tsx
@@ -1,9 +1,19 @@
+import Footer from '@/app/_components/Footer/Footer';
 import BusinessStatus from './_components/business-status/BusinessStatus';
+import HomeButton from './_components/home-buttons/HomeButton';
+import ProductList from './_components/product-list/ProductList';
+import Header from '@/app/_components/Header/Header';
 
 export default function Page() {
   return (
-    <main className="flex flex-col items-center px-5">
-      <BusinessStatus />
-    </main>
+    <>
+      <Header />
+      <main className="mt-2 flex flex-col items-center px-5">
+        <BusinessStatus />
+        <HomeButton />
+        <ProductList />
+      </main>
+      <Footer />
+    </>
   );
 }
