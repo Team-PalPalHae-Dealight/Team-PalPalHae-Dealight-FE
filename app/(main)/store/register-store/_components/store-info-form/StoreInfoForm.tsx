b4bdf66d9d9fe4bf72ec65f456ae205d8ea54d8e
Lim_JiSeon <dpf226@naver.com>
2023-11-17 15:43:19 +0900

    feat: 업체 정보 폼 react hook form 구현

--- app/(main)/store/register-store/_components/store-info-form/StoreInfoForm.tsx
+++ app/(main)/store/register-store/_components/store-info-form/StoreInfoForm.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { ErrorMessage } from '@hookform/error-message';
 import { object } from 'yup';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { ERROR_MESSAGE } from '@/app/_constants/errorMessage';
 import PrimaryButton from '@/app/_components/PrimaryButton/PrimaryButton';
 import pageRoute from '@/app/_constants/path';
@@ -10,6 +11,7 @@ import AddressButton from '@/app/_components/AddressButton/AddressButton';
 import { useState, useEffect } from 'react';
 import { isValidStoreName, isValidStorePhone } from '../../_utils/validate';
 import LocalStorage from '@/app/_utils/localstorage';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 type initialValuesType = {
   storeName: string;
@@ -24,24 +26,27 @@ const StoreInfoForm = () => {
   const [click, setClick] = useState(false);
   const [next, setNext] = useState(false);
 
-  const initialValues = {
-    storeName: '',
-    storePhone: '',
-  };
-
   const schema = object().shape({
     storeName: isValidStoreName(),
     storePhone: isValidStorePhone(),
   });
 
-  const submitForm = (values: initialValuesType) => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<initialValuesType>({ resolver: yupResolver(schema) });
+
+  const onSubmit: SubmitHandler<initialValuesType> = () => {
+    /** @todo api 연결 */
+    const { storeName, storePhone } = watch();
     const address = LocalStorage.getItem('dealight-storeAddress');
 
     if (address) setAddressError(false);
-
-    if (!addressError && values.storeName && values.storePhone && next) {
-      LocalStorage.setItem('dealight-storeName', values.storeName);
-      LocalStorage.setItem('dealight-storePhone', values.storePhone);
+    if (!addressError && storeName && storePhone && next) {
+      LocalStorage.setItem('dealight-storeName', storeName);
+      LocalStorage.setItem('dealight-storePhone', storePhone);
 
       router.push(pageRoute.store.registerStoreTime());
     }
@@ -64,89 +69,83 @@ const StoreInfoForm = () => {
 
   return (
     <div className="flex w-full flex-col items-center">
-      <Formik
-        initialValues={initialValues}
-        validationSchema={schema}
-        onSubmit={submitForm}
-      >
-        {formik => {
-          return (
-            <Form className="mt-40 w-full">
-              <div>
-                <label className="text-xs font-semibold" htmlFor="storeName">
-                  업체 이름
-                </label>
-                <Field
-                  type="text"
-                  name="storeName"
-                  className={`h-12 w-full rounded text-base text-black ${
-                    formik.errors.storeName ? 'border-red' : 'border-yellow'
-                  } cursor-pointer bg-white pl-3 outline-none focus:border-2`}
-                  placeholder="세븐일레븐"
-                />
-                <ErrorMessage
-                  name="storeName"
-                  component="span"
-                  className="w-full text-left text-xs text-red"
-                />
-              </div>
-              <div className="mt-5">
-                <label
-                  className="mt-5 text-xs font-semibold text-black"
-                  htmlFor="storePhone"
-                >
-                  업체 전화번호
-                </label>
-                <Field
-                  type="text"
-                  name="storePhone"
-                  className={`h-12 w-full rounded text-base text-black ${
-                    formik.errors.storePhone ? 'border-red' : 'border-yellow'
-                  } cursor-pointer bg-white pl-3 outline-none focus:border-2`}
-                  placeholder="0311234567"
-                />
-                <ErrorMessage
-                  name="storePhone"
-                  component="span"
-                  className="w-full text-left text-xs text-red"
-                />
-              </div>
-              <div className="mt-5">
-                <label className="text-xs font-semibold" htmlFor="storeAddress">
-                  업체 주소
-                </label>
-                <div className="flex gap-x-2">
-                  <div
-                    className={`base-2/5 h-12 w-full rounded bg-white py-3 pl-3 text-base text-black outline-none`}
-                  >
-                    {address}
-                  </div>
-                  <AddressButton
-                    type="button"
-                    getAddress={handleAddressButton}
-                    className="base-3/5 h-12 min-w-fit rounded bg-yellow px-1 text-sm"
-                  >
-                    주소찾기
-                  </AddressButton>
-                </div>
-                {click && addressError && (
-                  <div className="w-full text-left text-xs text-red">
-                    {ERROR_MESSAGE.STORE_REQUIRED}
-                  </div>
-                )}
-              </div>
-              <div className="mb-7 mt-24 flex justify-center gap-3">
-                <div className="h-2.5 w-2.5 rounded-full bg-dark-gray"></div>
-                <div className="h-2.5 w-2.5 rounded-full bg-yellow"></div>
-                <div className="h-2.5 w-2.5 rounded-full bg-dark-gray"></div>
-              </div>
-              <PrimaryButton type="submit" onClick={() => setNext(true)}>
-                다음
-              </PrimaryButton>
-            </Form>
-          );
-        }}
-      </Formik>
+      <form className="mt-40 w-full" onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label className="text-xs font-semibold" htmlFor="storeName">
+            업체 이름
+          </label>
+          <input
+            type="text"
+            className={`h-12 w-full rounded text-base text-black ${
+              errors.storeName ? 'border-red' : 'border-yellow'
+            } cursor-pointer bg-white pl-3 outline-none focus:border-2`}
+            placeholder="업체명"
+            {...register('storeName')}
+          />
+          <ErrorMessage
+            errors={errors}
+            name="storeName"
+            render={({ message }) => (
+              <div className="w-full text-left text-xs text-red">{message}</div>
+            )}
+          />
+        </div>
+        <div className="mt-5">
+          <label
+            className="mt-5 text-xs font-semibold text-black"
+            htmlFor="storePhone"
+          >
+            업체 전화번호
+          </label>
+          <input
+            type="text"
+            className={`h-12 w-full rounded text-base text-black ${
+              errors.storePhone ? 'border-red' : 'border-yellow'
+            } cursor-pointer bg-white pl-3 outline-none focus:border-2`}
+            placeholder="0311234567"
+            {...register('storePhone')}
+          />
+          <ErrorMessage
+            errors={errors}
+            name="storePhone"
+            render={({ message }) => (
+              <div className="w-full text-left text-xs text-red">{message}</div>
+            )}
+          />
+        </div>
+        <div className="mt-5">
+          <label className="text-xs font-semibold" htmlFor="storeAddress">
+            업체 주소
+          </label>
+          <div className="flex gap-x-2">
+            <div
+              className={`base-2/5 h-12 w-full rounded bg-white py-3 pl-3 text-base text-black outline-none`}
+            >
+              {address}
+            </div>
+            <AddressButton
+              type="button"
+              getAddress={handleAddressButton}
+              className="base-3/5 h-12 min-w-fit rounded bg-yellow px-1 text-sm"
+            >
+              주소찾기
+            </AddressButton>
+          </div>
+          {click && addressError && (
+            <div className="w-full text-left text-xs text-red">
+              {ERROR_MESSAGE.STORE_REQUIRED}
+            </div>
+          )}
+        </div>
+        <div className="mb-7 mt-24 flex justify-center gap-3">
+          <div className="h-2.5 w-2.5 rounded-full bg-dark-gray"></div>
+          <div className="h-2.5 w-2.5 rounded-full bg-yellow"></div>
+          <div className="h-2.5 w-2.5 rounded-full bg-dark-gray"></div>
+        </div>
+        <PrimaryButton type="submit" onClick={() => setNext(true)}>
+          다음
+        </PrimaryButton>
+      </form>
     </div>
   );
 };
