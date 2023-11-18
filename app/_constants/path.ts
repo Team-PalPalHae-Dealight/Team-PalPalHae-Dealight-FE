1fc7593897001b2130506dad80102ee256684d8b
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-11-18 20:17:42 +0900

    feat: 업체 측 상품 관리하기 페이지 폴더 생성 및 라우팅 처리

--- app/_constants/path.ts
+++ app/_constants/path.ts
@@ -28,6 +28,7 @@ const pageRoute = {
     registerStoreNumber: () => '/store/register-store/store-number',
     registerStoreInfo: () => '/store/register-store/store-info',
     registerStoreTime: () => '/store/register-store/store-time',
+    itemManage: () => '/store/item-manage',
   },
 };
 
