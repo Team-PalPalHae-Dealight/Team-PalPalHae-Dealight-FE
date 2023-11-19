04b528a9c42d1735590777a6fda3b655e4a1bbb2
Lim_JiSeon <dpf226@naver.com>
2023-11-20 07:52:18 +0900

    feat: 로그아웃 버튼 구현

--- app/(main)/store/my-page/[id]/page.tsx
+++ app/(main)/store/my-page/[id]/page.tsx
@@ -1,14 +1,16 @@
 import Footer from '@/app/_components/Footer/Footer';
 import Header from '@/app/_components/Header/Header';
 import ImageUploader from './_components/image-uploader/ImageUploader';
+import LogoutButton from './_components/logout-button/LogoutButton';
 
 export default function Page({ params }: { params: { slug: string } }) {
   console.log(params);
   return (
     <>
       <Header />
-      <main className="px-5 pt-5">
+      <main className="flex w-full flex-col px-5 pt-5">
         <ImageUploader />
+        <LogoutButton />
       </main>
       <Footer />
     </>
