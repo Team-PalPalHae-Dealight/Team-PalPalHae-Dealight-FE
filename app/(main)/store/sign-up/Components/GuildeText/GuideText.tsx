5759f62ed45e0bce8e91718693438062c4e7502c
euan-lee <maplestory1419@naver.com>
2023-11-20 13:46:04 +0900

    refactor: 변수명수정 파일 삭제

--- app/(main)/store/sign-up/Components/GuildeText/GuideText.tsx
+++ app/(main)/store/sign-up/Components/GuildeText/GuideText.tsx
@@ -1,6 +1,6 @@
 const GuideText = () => {
   return (
-    <div className="text-l flex flex-col items-center py-20 text-center font-semibold">
+    <div className="flex flex-col items-center py-20 text-center text-lg font-semibold">
       <div>간편하게 가입하고</div>
       <div>딜라잇(Dealight) 서비스를 이용해보세요</div>
     </div>
