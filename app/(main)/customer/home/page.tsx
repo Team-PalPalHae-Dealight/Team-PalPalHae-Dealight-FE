5ff174dc6f27e18766dd5a290f70ff46084b20a8
euan-lee <maplestory1419@naver.com>
2023-11-20 23:17:06 +0900

    feat: 헤더 주솟값 전역 컨텍스트로 구현

--- app/(main)/customer/home/page.tsx
+++ app/(main)/customer/home/page.tsx
@@ -4,16 +4,20 @@ import KakaoMap from '@/app/_components/KakaoMap/KakaoMap';
 import TodayDealight from '@/app/(main)/customer/home/_components/TodayDealight';
 import CustomerHeader from '@/app/_components/Header/CustomerHeader';
 import Footer from '@/app/_components/Footer/Footer';
+import { useAddress } from '@/app/_providers/AddressProvider';
 
 export default function Page() {
+  const { address } = useAddress();
   return (
     <>
       <CustomerHeader />
       <div className="mx-5">
         <div className="my-3 rounded">
           <KakaoMap
-            onClickCurrentPosition={() => console.log('현재 위치 클릭')}
-            onClickPosition={() => console.log('marker')}
+            onClickCurrentPosition={() =>
+              console.log(`현재 위치 클릭:${address}`)
+            }
+            onClickPosition={() => console.log('marker', address)}
             positions={[
               { lat: 33.450705, lng: 126.56956, title: '카카오' },
               { lat: 33.450936, lng: 126.569477, title: '생태연못' },
