9f08a3db5b72a38afcadaf1a586766d50ab93d35
JongHyun <108605838+jonghyunlee95@users.noreply.github.com>
2023-11-22 16:14:46 +0900

    고객 측 주문 내역 페이지 api 연결 작업 (#186)
    * feat: api 연결 시도
    
    * feat: api 연결
    
    * feat: api호출 파일 폴더 수정
    
    * refactor: 리뷰 반영

--- app/(main)/customer/order-list/[id]/_components/order-list-card/OrderListCard.tsx
+++ app/(main)/customer/order-list/[id]/_components/order-list-card/OrderListCard.tsx
@@ -1,38 +1,53 @@
 'use client';
 
-import { ResponseItemTypes } from '@/app/_components/infinite-scroll/fetchData';
 import Link from 'next/link';
+import { ResponseItemType } from '../../_services/getOrderList';
+import pageRoute from '@/app/_constants/path';
 
 export type OrderListCardPropsType = {
-  items?: ResponseItemTypes[] | null;
+  items?: ResponseItemType[] | [];
 };
 
 const OrderListCard = ({ items }: OrderListCardPropsType) => {
-  const onClickCardLink = () => {
-    sessionStorage.setItem('scrollY', JSON.stringify(window.scrollY));
-  };
   return (
     <>
       {items ? (
         items.map(item => {
           return (
-            <Link key={item.id} href={'/asd'} onClick={onClickCardLink}>
+            <Link
+              key={item.orderId}
+              href={pageRoute.customer.orderDetail(String(item.orderId))}
+            >
               <div
                 className=" mb-3 rounded-md bg-white p-2"
                 style={{ boxShadow: '0px 0px 4px 0px rgb(0, 0, 0, 0.1)' }}
               >
                 <div className="flex text-xs text-dark-gray">
                   <div className="flex justify-between">
-                    <div>2023.10.20.</div>
+                    {/* @todo 시간 수정해야 함 */}
+                    <div>{item.createdAt}</div>
                   </div>
-                  <div className="ml-1">17:32</div>
                 </div>
                 <div className="flex items-center gap-1 text-black">
-                  <div className="text-sm">달콤한 도너츠 외 3개</div>
-                  <div className=" text-xs">11000 원</div>
+                  <div className="text-sm">
+                    {item.orderProductsRes.orderProducts[0].name} 외
+                    {item.orderProductsRes.orderProducts.length - 1}개
+                  </div>
+                  <div className=" text-xs">{item.totalPrice} 원</div>
                 </div>
                 <div className="flex items-center text-black">
-                  <div className="text-sm text-[#0338FF]">주문 완료</div>
+                  {item.status === '주문 접수' && (
+                    <div className="text-sm text-green">{item.status}</div>
+                  )}
+                  {item.status === '주문 확인' && (
+                    <div className="text-sm text-orange">{item.status}</div>
+                  )}
+                  {item.status === '주문 완료' && (
+                    <div className="text-sm text-blue">{item.status}</div>
+                  )}
+                  {item.status === '주문 취소' && (
+                    <div className="text-sm text-red">{item.status}</div>
+                  )}
                 </div>
               </div>
             </Link>
