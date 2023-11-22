8095a2633f13f9602a7950845bd601d33d9fcb27
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-11-22 21:17:18 +0900

    feat: 업체 측 홈페이지 api연결

--- app/(main)/store/home/_components/product-list/ItemCards.tsx
+++ app/(main)/store/home/_components/product-list/ItemCards.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import donut from '@/app/_assets/images/donut.png';
-import { ResponseItemTypes } from '../../fetchData';
+import { ResponseItemTypes } from '../../_services/getItemList';
+import pageRoute from '@/app/_constants/path';
 
 export type ItemTypes = {
-  items?: ResponseItemTypes[] | null;
+  items?: ResponseItemTypes[] | [];
 };
 
 const ItemCards = ({ items }: ItemTypes) => {
@@ -14,35 +14,40 @@ const ItemCards = ({ items }: ItemTypes) => {
       {items ? (
         items.map(item => {
           return (
-            /**
-             * @todo
-             * 라우팅 처리 수정 필요
-             */
-            <Link key={item.id} href={'/'}>
+            <Link
+              key={item.itemId}
+              href={pageRoute.store.itemDetail(String(item.itemId))}
+            >
               <div
                 className="mb-2 flex h-20 items-center justify-between rounded bg-white p-4"
                 style={{ boxShadow: '0px 0px 4px 0px rgb(0, 0, 0, 0.1)' }}
               >
                 <div>
-                  <Image width={60} height={60} src={donut} alt="donut" />
+                  <Image width={60} height={60} src={item.image} alt="donut" />
                 </div>
                 <div className="flex items-center">
                   <div className="flex flex-col gap-1">
-                    <div className="text-sm font-semibold">달콤한 도너츠</div>
+                    <div className="text-sm font-semibold">{item.itemName}</div>
                     <div className="flex gap-1 text-xs font-semibold">
                       <div>재고: </div>
-                      <div className="text-red">3개</div>
+                      <div className="text-red">{item.stock}개</div>
                     </div>
                   </div>
                   <div className="ml-12 flex flex-col gap-1">
                     <div className="flex justify-end text-xs text-dark-gray line-through">
-                      <div>3000 원</div>
+                      <div>{item.originalPrice} 원</div>
                     </div>
                     <div className="flex items-center justify-end gap-2">
                       <div className="text-sm font-semibold text-red">
-                        할인 50%
+                        할인
+                        {(
+                          ((item.originalPrice - item.discountPrice) /
+                            item.originalPrice) *
+                          100
+                        ).toFixed(0)}
+                        %
                       </div>
-                      <div className="text-xs">1500 원</div>
+                      <div className="text-xs">{item.discountPrice} 원</div>
                     </div>
                   </div>
                 </div>
