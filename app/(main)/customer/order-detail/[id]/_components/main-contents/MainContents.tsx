621fbfce6d3738784c5f3ce91788c9b663f160dc
Lim_JiSeon <dpf226@naver.com>
2023-11-23 09:08:26 +0900

    feat: 고객 주문 상세 페이지 구현 완료 및 get, cancel api 연결

--- app/(main)/customer/order-detail/[id]/_components/main-contents/MainContents.tsx
+++ app/(main)/customer/order-detail/[id]/_components/main-contents/MainContents.tsx
@@ -4,23 +4,65 @@ import CustomerHeader from '@/app/_components/Header/CustomerHeader';
 import ProductList from '../product-list/ProductList';
 import OrderResult from '@/app/_components/order-result/OrderResult';
 import CustomerFooter from '@/app/_components/Footer/CustomerFooter';
+import { useCallback, useEffect, useState } from 'react';
+import { getOrder } from '@/app/_services/order/getOrder';
+import ReviewButton from '../review-button/ReviewButton';
 
-const MainContents = () => {
-  /** @TODO API 연결 후 res로 받은 데이터로 초기화시키기 */
-  const data = {
+type OrderResultPropsType = {
+  storeName: string;
+  totalCount: string;
+  totalPrice: string;
+  arriveTime: string;
+  useName: string;
+  comments: string;
+};
+
+type MainContentsPropsType = {
+  orderId: number;
+};
+
+const MainContents = ({ orderId }: MainContentsPropsType) => {
+  const [data, setData] = useState();
+  const [order, setOrder] = useState<OrderResultPropsType>();
+  console.log(data, order);
+
+  const sampleData = {
     storeName: '행복도너츠가게',
     totalCount: '4',
     totalPrice: '11000',
     arriveTime: '17 : 32',
     useName: '에프와 오프',
     comments: '빨리 갈께요!',
+    status: '주문 접수',
   };
+
+  const getData = useCallback(async () => {
+    const res = await getOrder(orderId);
+    console.log(res);
+
+    /** @todo sampleData 자리에 order로 초기화, status 자리에 data.status 초기화 */
+    setData(res);
+    setOrder({
+      storeName: res.storeName,
+      totalCount: res.orderProductsRes.orderProducts.length,
+      totalPrice: res.totalPrice,
+      arriveTime: res.arrivalTime,
+      useName: res.memberNickName,
+      comments: res.demand,
+    });
+  }, [orderId]);
+
+  useEffect(() => {
+    getData();
+  }, [getData]);
+
   return (
     <main className="flex flex-col items-center">
       <CustomerHeader />
       <div className="w-full p-5">
         <ProductList />
-        <OrderResult data={data} />
+        <OrderResult data={sampleData} />
+        <ReviewButton status={sampleData.status} orderId={orderId} />
       </div>
       <CustomerFooter />
     </main>
---
+++ app/_services/order/getOrder.ts
@@ -0,0 +1,24 @@
+import { useRouter } from 'next/navigation';
+import { axiosInstance } from '../apiClient';
+import pageRoute from '@/app/_constants/path';
+import { useUserInfo } from '@/app/_providers/UserInfoProvider';
+
+export const getOrder = (orderId: number) => {
+  return axiosInstance
+    .get(`/orders/${orderId}`)
+    .then(function (response) {
+      console.log(response);
+      return response.data;
+    })
+    .catch(function (error) {
+      console.log(error);
+
+      const router = useRouter();
+      const { providerId } = useUserInfo();
+      router.push(
+        providerId
+          ? pageRoute.customer.orderList(String(providerId))
+          : pageRoute.customer.login()
+      );
+    });
+};
