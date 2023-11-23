1dbb12453a7ec7b4ee7c46a55630056cee4951fe
jgjgill <dbdltm22@naver.com>
2023-11-23 15:51:57 +0900

    feat: 로딩 관련 처리

--- app/(main)/customer/store-detail/[id]/_components/StoreDetail.tsx
+++ app/(main)/customer/store-detail/[id]/_components/StoreDetail.tsx
@@ -5,14 +5,15 @@ import ItemCards from './ItemCards';
 import { ITEM_CARDS } from '../_mocks/itemMock';
 import Image from 'next/image';
 import { REVIEWS } from '../_mocks/reviewMock';
-// import { useGetStore } from '@/app/_hooks/query/store';
+import { useGetStore } from '@/app/_hooks/query/store';
+import { formatPhoneNumber } from '@/app/_utils/number';
 
 type StoreDetailPropsType = {
   storeId: string;
 };
 
 const StoreDetail = ({ storeId }: StoreDetailPropsType) => {
-  // const { data: store } = useGetStore({ storeId });
+  const { data: store, isLoading } = useGetStore({ storeId });
 
   const reviewImage = {
     '상품 상태가 좋아요': 'https://picsum.photos/id/75/200/300.jpg',
@@ -22,23 +23,30 @@ const StoreDetail = ({ storeId }: StoreDetailPropsType) => {
     '게시된 설명이 자세하고 실제 상품과 동일해요':
       'https://picsum.photos/id/95/200/300.jpg',
   };
-  console.log(storeId);
+
+  if (isLoading) {
+    return null;
+  }
+
+  const { closeTime, dayOff, name, openTime, telephone } = store!;
+  const dayOffText = dayOff.join(' ');
+
   return (
     <>
       <div className="w-full">
-        <h2 className="mb-4 text-xl font-semibold">{'달콤한 도너츠 가게'}</h2>
+        <h2 className="mb-4 text-xl font-semibold">{name}</h2>
         <div className="relative h-44 overflow-hidden rounded">
           <Image
             priority
             fill
-            alt={'달콤한 도너츠 가게'}
+            alt={name}
             src={'https://picsum.photos/id/395/200/300.jpg'}
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           />
         </div>
-        {/* <div className="flex h-44 items-center justify-center rounded bg-white text-dark-gray">
+        <div className="flex h-44 items-center justify-center rounded bg-white text-dark-gray">
           등록된 이미지가 없습니다
-        </div> */}
+        </div>
       </div>
 
       <div className="min-h-42.5 w-full rounded bg-white p-4">
@@ -55,9 +63,9 @@ const StoreDetail = ({ storeId }: StoreDetailPropsType) => {
       <div className="min-h-42.5 w-full rounded bg-white p-4">
         <h2 className="pb-2.5 text-lg font-semibold">업체 정보</h2>
         <div className="flex flex-col gap-1.5 text-sm font-semibold">
-          <div>영업 시간 : {'10 : 00 ~ 22 : 00'}</div>
-          <div>휴무일 : {'연중 무휴'}</div>
-          <div>연락처 : {'031 - 123 - 1234'}</div>
+          <div>영업 시간 : {`${openTime} ~ ${closeTime}`}</div>
+          <div>휴무일 : {dayOffText}</div>
+          <div>연락처 : {formatPhoneNumber(telephone)}</div>
         </div>
       </div>
 
