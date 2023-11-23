687d715ea04bf81cee04799e8fbfe05e8ce69435
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-11-23 14:50:01 +0900

    feat: mainContents추가

--- app/(main)/store/home/page.tsx
+++ app/(main)/store/home/page.tsx
@@ -1,23 +1,12 @@
-'use client';
-
-import BusinessStatus from './_components/business-status/BusinessStatus';
-import HomeButton from './_components/home-buttons/HomeButton';
-import ProductList from './_components/product-list/ProductList';
 import Header from '@/app/_components/Header/Header';
-import { useState } from 'react';
 import StoreFooter from '@/app/_components/Footer/StoreFooter';
+import MainContents from './_components/main-contents/MainContents';
 
 export default function Page() {
-  const [status, setStatus] = useState<'영업 중' | '영업 준비 중' | ''>('');
-
   return (
     <>
       <Header />
-      <main className="mt-2 flex flex-col items-center px-5">
-        <BusinessStatus setStatus={setStatus} />
-        <HomeButton />
-        <ProductList status={status} />
-      </main>
+      <MainContents />
       <StoreFooter />
     </>
   );
