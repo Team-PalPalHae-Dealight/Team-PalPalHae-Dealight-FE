54eb8ad90b5e5f7131103b90cc86c9fe6e10f721
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-11-24 17:58:57 +0900

    feat: 리스트 수정

--- app/(main)/customer/order-list/[id]/_components/order-list-card/OrderListCard.tsx
+++ app/(main)/customer/order-list/[id]/_components/order-list-card/OrderListCard.tsx
@@ -5,75 +5,59 @@ import { ResponseItemType } from '../../_services/getOrderList';
 import pageRoute from '@/app/_constants/path';
 
 export type OrderListCardPropsType = {
-  items?: ResponseItemType[] | [];
+  items: ResponseItemType[] | [];
 };
 
 const OrderListCard = ({ items }: OrderListCardPropsType) => {
-  const formattedDate = (dateString: string) => {
-    const matchArray = dateString.match(
-      /(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})/
-    );
-
-    if (matchArray) {
-      const [, year, month, day, hours, minutes] = matchArray;
-      return `${year}.${month}.${day} ${hours}.${minutes}`;
-    } else {
-      console.error('Invalid date string format');
-      return null;
-    }
-  };
+  console.log(items);
   return (
     <>
-      {items ? (
-        items.map(item => {
-          return (
-            <Link
-              key={item.orderId}
-              href={pageRoute.customer.orderDetail(String(item.orderId))}
+      {items.map(item => {
+        return (
+          <Link
+            key={item.orderId}
+            href={pageRoute.customer.orderDetail(String(item.orderId))}
+          >
+            <div
+              className=" mb-3 rounded-md bg-white p-2"
+              style={{ boxShadow: '0px 0px 4px 0px rgb(0, 0, 0, 0.1)' }}
             >
-              <div
-                className=" mb-3 rounded-md bg-white p-2"
-                style={{ boxShadow: '0px 0px 4px 0px rgb(0, 0, 0, 0.1)' }}
-              >
-                <div className="flex text-xs text-dark-gray">
-                  <div className="flex justify-between">
-                    <div>{formattedDate(item.createdAt)}</div>
-                  </div>
-                </div>
-                <div className="flex items-center gap-1 text-black">
-                  {item.orderProductsRes.orderProducts.length > 1 ? (
-                    <div className="text-sm">
-                      {item.orderProductsRes.orderProducts[0].name} 외
-                      {item.orderProductsRes.orderProducts.length - 1}개
-                    </div>
-                  ) : (
-                    <div className="text-sm">
-                      {item.orderProductsRes.orderProducts[0].name}
-                    </div>
-                  )}
-                  <div className=" text-xs">{item.totalPrice} 원</div>
-                </div>
-                <div className="flex items-center text-black">
-                  {item.status === '주문 접수' && (
-                    <div className="text-sm text-green">{item.status}</div>
-                  )}
-                  {item.status === '주문 확인' && (
-                    <div className="text-sm text-orange">{item.status}</div>
-                  )}
-                  {item.status === '주문 완료' && (
-                    <div className="text-sm text-blue">{item.status}</div>
-                  )}
-                  {item.status === '주문 취소' && (
-                    <div className="text-sm text-red">{item.status}</div>
-                  )}
+              <div className="flex text-xs text-dark-gray">
+                <div className="flex justify-between">
+                  <div>{item.createdAt.split(' ')[0]}</div>
                 </div>
               </div>
-            </Link>
-          );
-        })
-      ) : (
-        <></>
-      )}
+              <div className="flex items-center gap-1 text-black">
+                {item.orderProductsRes.orderProducts.length > 1 ? (
+                  <div className="text-sm">
+                    {item.orderProductsRes.orderProducts[0].name} 외
+                    {item.orderProductsRes.orderProducts.length - 1}개
+                  </div>
+                ) : (
+                  <div className="text-sm">
+                    {item.orderProductsRes.orderProducts[0].name}
+                  </div>
+                )}
+                <div className=" text-xs">{item.totalPrice} 원</div>
+              </div>
+              <div className="flex items-center text-black">
+                {item.status === '주문 접수' && (
+                  <div className="text-sm text-green">{item.status}</div>
+                )}
+                {item.status === '주문 확인' && (
+                  <div className="text-sm text-orange">{item.status}</div>
+                )}
+                {item.status === '주문 완료' && (
+                  <div className="text-sm text-blue">{item.status}</div>
+                )}
+                {item.status === '주문 취소' && (
+                  <div className="text-sm text-red">{item.status}</div>
+                )}
+              </div>
+            </div>
+          </Link>
+        );
+      })}
     </>
   );
 };
--- app/(main)/store/order-list/[id]/_components/order-list-card/OrderListCard.tsx
+++ app/(main)/store/order-list/[id]/_components/order-list-card/OrderListCard.tsx
@@ -7,19 +7,6 @@ export type OrderListCardPropsType = {
 };
 
 const OrderListCard = ({ items }: OrderListCardPropsType) => {
-  const formattedDate = (dateString: string) => {
-    const matchArray = dateString.match(
-      /(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})/
-    );
-
-    if (matchArray) {
-      const [, year, month, day, hours, minutes] = matchArray;
-      return `${year}.${month}.${day} ${hours}.${minutes}`;
-    } else {
-      console.error('Invalid date string format');
-      return null;
-    }
-  };
   return (
     <>
       {items.map(item => {
@@ -33,7 +20,7 @@ const OrderListCard = ({ items }: OrderListCardPropsType) => {
               style={{ boxShadow: '0px 0px 4px 0px rgb(0, 0, 0, 0.1)' }}
             >
               <div className="flex text-xs text-dark-gray">
-                <div>{formattedDate(item.createdAt)}</div>
+                <div>{item.createdAt.split(' ')[0]}</div>
               </div>
               <div className="flex items-center gap-1 text-black">
                 {item.orderProductsRes.orderProducts.length > 1 ? (
@@ -50,8 +37,8 @@ const OrderListCard = ({ items }: OrderListCardPropsType) => {
               </div>
               <div className="flex items-center justify-between text-black">
                 <div className="text-xs">
-                  도착예정 시간: {item.arrivalTime.slice(0, -3).split(':')[0]}시
-                  {item.arrivalTime.slice(0, -3).split(':')[1]}분
+                  도착예정 시간: {item.arrivalTime.split(':')[0]}시
+                  {item.arrivalTime.split(':')[1]}분
                 </div>
                 {item.status === '주문 접수' && (
                   <div className="text-sm text-green">주문 접수</div>
