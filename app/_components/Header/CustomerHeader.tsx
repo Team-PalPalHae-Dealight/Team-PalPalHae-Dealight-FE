70a3ce790109f758d35a176f3e38f4a109977e65
(euan) <62791913+euan-lee@users.noreply.github.com>
2023-11-25 12:38:08 +0900

    feat: 헤더에 주소값 fetch api 적용 (#219)
    * feat: 헤더에 주소값 fetch api 적용
    
    * refactor:헤더작업

--- app/_components/Header/CustomerHeader.tsx
+++ app/_components/Header/CustomerHeader.tsx
@@ -7,21 +7,57 @@ import pageRoute from '@/app/_constants/path';
 import AddressButton from '../AddressButton/AddressButton';
 import Triangle from './assets/triangle.svg';
 import dynamic from 'next/dynamic';
+import { useAuth } from '@/app/_providers/AuthProvider';
+import { useEffect, useState } from 'react';
+import { axiosInstance } from '@/app/_services/apiClient';
+import { useUserInfo } from '@/app/_providers/UserInfoProvider';
+import useCoordinate from '@/app/_hooks/useCoordinate';
 import { useAddress } from '@/app/_providers/AddressProvider';
 
 const LoginHeader = dynamic(() => import('./LoginHeader'), { ssr: false });
 
 const CustomerHeader = () => {
-  const { address, setAddress } = useAddress();
+  const { loggedIn } = useAuth();
+  const state = useUserInfo();
+  const [address, setAddress] = useState(state.address.name);
+  const { lng, lat } = useCoordinate(address);
+  const { getAddress } = useAddress();
+
+  useEffect(() => {
+    if (!state || !state.address || !state.address.name) return;
+
+    setAddress(state.address.name);
+  }, [state]);
+
+  useEffect(() => {
+    const updateAddress = async () => {
+      try {
+        const res = await axiosInstance.patch('/members/addresses', {
+          name: address,
+          xCoordinate: lng,
+          yCoordinate: lat,
+        });
+        console.log(res);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    updateAddress();
+    getAddress(address);
+  }, [address, lat, lng, getAddress]);
 
   return (
     <div className="align-center space-between text-l sticky top-0 z-50 box-border flex h-16 w-full justify-between border-b-1 border-dark-gray/30 bg-light-gray px-3 py-4 font-semibold text-black">
       <div className="flex flex-row items-center">
         <div>
-          <AddressButton getAddress={addressVal => setAddress(addressVal)}>
+          <AddressButton
+            getAddress={addressVal => {
+              setAddress(addressVal);
+            }}
+          >
             <div className="flex flex-row">
               <div className=" w-28 overflow-hidden text-ellipsis whitespace-nowrap">
-                {address}
+                {loggedIn ? address : '강남역 2번출구'}
               </div>
               <div>
                 <Triangle className="h-6 w-6 px-1 py-1.5" />
--- app/_providers/UserInfoProvider.tsx
+++ app/_providers/UserInfoProvider.tsx
@@ -6,11 +6,20 @@ import { axiosInstance } from '../_services/apiClient';
 import { useAuth } from './AuthProvider';
 import LocalStorage from '../_utils/localstorage';
 
+type AddressPropstype = {
+  name: string;
+  xCoordinate: number;
+  yCoordinate: number;
+};
+
 type DefaultContextType = {
   providerId: number | null;
   storeId: number | null;
   nickName: string | null;
   role: 'store' | 'member' | null;
+  realName: string | null;
+  phoneNumber: number | null;
+  address: AddressPropstype;
 } | null;
 
 const UserInfoContext = createContext<DefaultContextType>(null);
@@ -20,7 +29,7 @@ async function getUser(): Promise<DefaultContextType> {
     .get(`${process.env.NEXT_PUBLIC_API_URL}/members/profiles`)
     .then(res => res.data);
 
-  const { nickName, providerId, role } = data;
+  const { nickName, providerId, role, realName, phoneNumber, address } = data;
 
   let storeId = null;
 
@@ -29,11 +38,19 @@ async function getUser(): Promise<DefaultContextType> {
       .get(`${process.env.NEXT_PUBLIC_API_URL}/stores/confirm`)
       .then(res => res.data.storeId);
   }
-
+  console.log('api called');
   /**
    * @description role에 고객 or 업체가 들어온다. 이 값을 통해 라우팅 처리가 이루어지게 된다.
    */
-  return { role, nickName, providerId, storeId };
+  return {
+    role,
+    nickName,
+    providerId,
+    storeId,
+    realName,
+    phoneNumber,
+    address,
+  };
 }
 
 export const UserInfoProvider = ({
@@ -55,6 +72,13 @@ export const UserInfoProvider = ({
       role: null,
       storeId: null,
       providerId: null,
+      realName: null,
+      phoneNumber: null,
+      address: {
+        name: '광화문',
+        xCoordinate: 126.976372775,
+        yCoordinate: 37.571648599,
+      },
     },
     enabled:
       !!LocalStorage.getItem('dealight-accessToken') ||
