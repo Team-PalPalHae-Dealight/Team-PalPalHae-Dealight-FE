ac4ffd321fa15c9034d9ddffaaaf7cb9a4ecf315
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-11-27 19:12:46 +0900

    feat: 업체 측 홈페이지 리스트 리팩토링

--- app/(main)/store/home/_components/product-list/OpenStoreItems.tsx
+++ app/(main)/store/home/_components/product-list/OpenStoreItems.tsx
@@ -1,12 +1,21 @@
 import { useGetMyStoreItems } from '@/app/_hooks/query/item';
 import ItemCards from './ItemCards';
+import Spinner from '@/app/_components/spinner/Spinner';
 
 const OpenStoreItems = () => {
-  const { data: storeItems, ref } = useGetMyStoreItems({ size: 5 });
+  const {
+    data: storeItems,
+    ref,
+    isFetchingNextPage,
+  } = useGetMyStoreItems({ size: 5 });
   return (
     <>
       <ItemCards items={storeItems} />
-
+      {isFetchingNextPage && (
+        <div className="my-1 flex items-center justify-center">
+          <Spinner />
+        </div>
+      )}
       <div ref={ref} className="h-4" />
 
       {storeItems.length === 0 && (
