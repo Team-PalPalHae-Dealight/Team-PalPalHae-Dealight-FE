2ba29e9dfdc2021a04355aadab6585cb19a1ceb1
jgjgill <dbdltm22@naver.com>
2023-11-29 00:58:58 +0900

    feat: 헤더 변경

--- app/_components/Header/Header.tsx
+++ app/_components/Header/Header.tsx
@@ -4,13 +4,13 @@ import pageRoute from '@/app/_constants/path';
 
 const Header = () => {
   return (
-    <div className="first-line:text-l sticky top-0 z-50 box-border flex h-16 w-full items-center justify-start border-b-1 border-dark-gray/30 bg-light-gray px-8 py-3 font-semibold text-black">
+    <div className="first-line:text-l sticky top-0 z-50 box-border h-16 w-full border-b-1 border-dark-gray/30 bg-light-gray px-5 py-3 font-semibold">
       <Link
         href={pageRoute.store.home()}
-        className="flex flex-row items-center justify-start"
+        className="flex h-full w-full items-center gap-1"
       >
-        <DelightEmoji className="h-6 w-6" />
-        <div className=" py-1">딜라잇(Dealight)</div>
+        <DelightEmoji />
+        <h1 className="text-base font-semibold">Dealight</h1>
       </Link>
     </div>
   );
