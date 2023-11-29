be4846b845037dfdd8cd0af657c41b98e4bdf2b4
jgjgill <dbdltm22@naver.com>
2023-11-30 02:50:56 +0900

    test: 스타일 테스트

---
+++ app/(main)/store/item-detail/[id]/layout.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+// import { useAuth } from '@/app/_providers/AuthProvider';
+// import { useUserInfo } from '@/app/_providers/UserInfoProvider';
+// import { useRouter } from 'next/navigation';
+// import { useEffect } from 'react';
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  // const { role } = useUserInfo();
+  // const { loggedIn } = useAuth();
+  // const router = useRouter();
+
+  // useEffect(() => {
+  //   if (role === 'member' || !loggedIn) {
+  //     router.push('/');
+  //   }
+  // }, [loggedIn, router, role]);
+
+  // if (!role) {
+  //   return null;
+  // }
+
+  return <div>{children}</div>;
+}
--- app/(main)/store/item-edit/[id]/_components/ItemEdit.tsx
+++ app/(main)/store/item-edit/[id]/_components/ItemEdit.tsx
@@ -120,10 +120,10 @@ const ItemEdit = ({ itemId }: ItemEditPropsType) => {
             />
           </div>
 
-          <div className="flex w-full max-w-[280px] flex-col gap-3">
+          <div className="flex flex-col gap-3">
             <input
               {...register('itemName', { required: '값을 입력해주세요.' })}
-              className="rounded border border-transparent py-3.5 pl-3 focus:border-yellow"
+              className="max-w-[180px] rounded border border-transparent py-3.5 pl-3 focus:border-yellow"
               placeholder="상품명"
             />
 
@@ -137,7 +137,7 @@ const ItemEdit = ({ itemId }: ItemEditPropsType) => {
                     !isNaN(Number(value)) || '숫자로 입력해주세요.',
                 },
               })}
-              className="rounded border border-transparent py-3.5 pl-3 focus:border-yellow"
+              className="max-w-[180px] rounded border border-transparent py-3.5 pl-3 focus:border-yellow"
               placeholder="재고"
             />
 
