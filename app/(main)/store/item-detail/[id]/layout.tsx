ee6662757e4cf9a49af064da7a3271fcb48d47cc
jgjgill <dbdltm22@naver.com>
2023-11-30 03:02:38 +0900

    chore: 스타일 테스트 진행

--- app/(main)/store/item-detail/[id]/layout.tsx
+++ app/(main)/store/item-detail/[id]/layout.tsx
@@ -1,24 +1,24 @@
 'use client';
 
-import { useAuth } from '@/app/_providers/AuthProvider';
-import { useUserInfo } from '@/app/_providers/UserInfoProvider';
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+// import { useAuth } from '@/app/_providers/AuthProvider';
+// import { useUserInfo } from '@/app/_providers/UserInfoProvider';
+// import { useRouter } from 'next/navigation';
+// import { useEffect } from 'react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const { role } = useUserInfo();
-  const { loggedIn } = useAuth();
-  const router = useRouter();
+  // const { role } = useUserInfo();
+  // const { loggedIn } = useAuth();
+  // const router = useRouter();
 
-  useEffect(() => {
-    if (role === 'member' || !loggedIn) {
-      router.push('/');
-    }
-  }, [loggedIn, router, role]);
+  // useEffect(() => {
+  //   if (role === 'member' || !loggedIn) {
+  //     router.push('/');
+  //   }
+  // }, [loggedIn, router, role]);
 
-  if (!role) {
-    return null;
-  }
+  // if (!role) {
+  //   return null;
+  // }
 
   return <div>{children}</div>;
 }
