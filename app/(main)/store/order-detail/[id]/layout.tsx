1acc36a37e1854dbb6d101c3281830a898b9979b
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-11-30 02:40:55 +0900

    feat: layout 수정

--- app/(main)/store/order-detail/[id]/layout.tsx
+++ app/(main)/store/order-detail/[id]/layout.tsx
@@ -3,9 +3,14 @@
 import { useUserInfo } from '@/app/_providers/UserInfoProvider';
 import { useRouter } from 'next/navigation';
 
-export default function Layout() {
+export default function Layout({ children }: { children: React.ReactNode }) {
   const { role } = useUserInfo();
   const router = useRouter();
 
-  if (role !== 'store') router.push('/');
+  if (role !== 'store') {
+    router.push('/');
+    return null;
+  }
+
+  return <>{children}</>;
 }
