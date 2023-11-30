34e2f0148f37ea4d1f5876208e559cb4c7a7b318
Lim_JiSeon <dpf226@naver.com>
2023-11-30 18:53:51 +0900

    style: 장바구니 input 디자인 수정

--- app/(main)/customer/cart/[id]/_components/order-information/OrderInformation.tsx
+++ app/(main)/customer/cart/[id]/_components/order-information/OrderInformation.tsx
@@ -23,50 +23,45 @@ const OrderInformation = ({ data }: OrderInformationPropsType) => {
       {data ? (
         <div className="min-h-64 w-full rounded bg-white text-sm font-semibold text-black">
           <div className="p-4">
-            <div className="pb-4 text-lg">주문 정보</div>
+            <div className="pb-4 text-xl">주문 정보</div>
             <div className="flex justify-between pb-2.5 pr-5">
-              <div className="w-3/5 max-w-[200px] overflow-hidden text-ellipsis whitespace-nowrap">
-                가게명 :{' '}
-                <span className="font-normal text-dark-gray">
-                  {data[0]?.storeName}
-                </span>
+              <div className="w-3/5 max-w-[400px] overflow-hidden text-ellipsis whitespace-nowrap">
+                가게명 : <span>{data[0]?.storeName}</span>
               </div>
               <div>총 수량 : {total.totalCount} 개</div>
             </div>
             <div className="w-64 overflow-hidden text-ellipsis whitespace-nowrap pb-2.5">
               상품명 :{' '}
-              <span className="font-normal text-dark-gray">
+              <span>
                 {data.length === 1
                   ? data[0]?.itemName
                   : `${data[0]?.itemName} 외 ${data.length - 1} 개`}
               </span>
             </div>
             <div className="pb-2.5">
-              주문자 :{' '}
-              <span className="font-normal text-dark-gray">{nickName}</span>
+              주문자 : <span>{nickName}</span>
             </div>
             <div className="pb-2.5">
-              마감 시간 :{' '}
-              <span className="font-normal text-dark-gray">
-                {data[0].storeCloseTime}
-              </span>
+              마감 시간 : <span>{data[0].storeCloseTime}</span>
             </div>
-            <div className="flex w-full justify-between pb-2.5">
-              도착 예정 시간{' '}
-              <div className="flex-1 pl-2">
+            <div className="flex w-full flex-col pb-2.5">
+              <label className="w-full text-xs font-semibold">
+                도착 예정 시간
+              </label>
+              <div className="flex-1">
                 <input
                   type="time"
-                  className={`h-12 w-full cursor-pointer border-1 border-dark-gray text-xs font-normal text-black ${
-                    errors.arriveTime ? 'border-red' : 'border-dark-gray'
-                  } bg-white pl-3 outline-none`}
+                  className={`h-12 w-full cursor-pointer rounded border-1 border-gray text-xs font-normal text-black ${
+                    errors.arriveTime ? 'border-red' : 'border-white'
+                  } bg-gray pl-3 outline-none`}
                   {...register('arriveTime')}
                 />
               </div>
             </div>
-            <div className="flex w-full">
-              <span>요청 사항</span>
+            <div className="flex w-full flex-col">
+              <label className="w-full text-xs font-semibold">요청 사항</label>
               <input
-                className="ml-2 min-h-[40px] flex-1 border-1 border-solid border-dark-gray p-1.5 text-xs font-normal outline-none"
+                className="min-h-[80px] flex-1 rounded bg-gray p-1.5 text-xs font-normal outline-none"
                 type="text"
                 placeholder="선택 사항입니다."
                 defaultValue=""
