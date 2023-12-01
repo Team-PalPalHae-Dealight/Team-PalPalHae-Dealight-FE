bd42366ae222a3cd30b93c75ff529e026f946e8c
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-12-01 17:24:11 +0900

    refactor: 이미지 수정

--- app/(main)/store/item-detail/[id]/_components/ItemDetail.tsx
+++ app/(main)/store/item-detail/[id]/_components/ItemDetail.tsx
@@ -33,6 +33,7 @@ const ItemDetail = ({ itemId }: ItemDetailType) => {
             priority
             fill
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            className="object-cover"
           />
         </div>
 
