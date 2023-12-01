1f1abf184ba38ba26a0d9bfda2ba41a0f7f9d534
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-12-01 15:34:59 +0900

    refactor: 고객측 이미지 비율 수정

--- app/(main)/customer/search/_component/Itemcard/Itemcard.tsx
+++ app/(main)/customer/search/_component/Itemcard/Itemcard.tsx
@@ -21,13 +21,14 @@ const ItemCard = ({ image, name, closeTime, storeId }: ItemCardPropTypes) => {
         className="mb-2 flex items-center gap-2 rounded bg-white p-4"
         style={{ boxShadow: '0px 0px 4px 0px rgb(0, 0, 0, 0.1)' }}
       >
-        <div className="relative h-14 w-14 overflow-hidden rounded">
+        <div className="relative h-14 w-14 shrink-0 overflow-hidden rounded">
           <Image
             priority
             fill
             alt={name}
             src={String(image)}
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            objectFit="cover"
           />
         </div>
 
